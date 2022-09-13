8c0ee5cf3833204c79444579b5813f30c9fd7845
vam <656811+vamegh@users.noreply.github.com>
2022-09-13 10:06:40 +0100

    Refactor pretty much complete.
    Cleanups - removed pprints changed logging orders to make it less
    noisy although at info it is still pretty noisy.
    
    removed a bunch of commented out pprints and removed pprint calls.
    
    	modified:   libs/cloudformation.py
    	modified:   stack_rename

--- libs/cloudformation.py
+++ libs/cloudformation.py
@@ -1,6 +1,5 @@
 import logging
 import time
-from pprint import pprint
 import json
 from cfn_flip import to_json
 from copy import deepcopy
@@ -10,7 +9,7 @@ def verify_action(action, stack_name):
     logging.warning(f'You are about to perform: {action} on stack: {stack_name}')
     read_input = str(input('Are you sure you want to continue? (YES to do so): '))
     if read_input != 'YES':
-        logging.warning('Skipping step as requested')
+        logging.info('Skipping step as requested')
         return False
     return True
 
@@ -51,7 +50,7 @@ def stacks_importing_exports(aws_client, exports):
                       f'instead of the import name before continuing')
         logging.error(f'This will ensure the stack can be renamed without issue')
         continue_regardless = str(input(f'Do you wish to continue - '
-                                        f'even though this is guaranteed to fail? (Yes/no): '))
+                                        f'even though this is guaranteed to fail? (YES to do so): '))
         if continue_regardless != 'YES':
             logging.info('Wise Choice, exiting now')
             exit(0)
@@ -81,7 +80,6 @@ def remove_imports_from_stack_templates(aws_client, stacks_importing, exports):
             # could add possibly a config parameter for user to define the pattern
             # if they use sub for their imports etc - probably cleanest way of doing it
             # too much effort for now.
-            pprint(template)
 
 
 def detect_drift(aws_client, stack_id):
@@ -109,7 +107,6 @@ def detect_drift(aws_client, stack_id):
         resource_drifts_result = aws_client.cfn_stack_resource_drifts(stack_id, next_token=token)
         resource_drifts += resource_drifts_result['StackResourceDrifts']
 
-    # logging.info(f"Resource Drifts: {pprint(resource_drifts)}")
     return resource_drifts
 
 
@@ -149,16 +146,18 @@ def sanitize_template(data, template, resources, drifts):
             if k not in supported_imports.keys():
                 supported_imports[k] = template['Resources'][k]['Type']
 
-    logging.info(f'Supported imports: {supported_imports}')
-    logging.info(f'Unsupported imports: {non_importables}')
-    logging.info(f'Unsupported drifts: {non_driftables}')
+    logging.debug(f'Supported imports: {supported_imports}')
+    logging.debug(f'Unsupported imports: {non_importables}')
+    logging.debug(f'Unsupported drifts: {non_driftables}')
     return supported_imports, non_importables, non_driftables, sanitized_template
 
 
 def sanitize_resources(data, drifts, template):
     import_resources = []
+    import_resource_counter = 0
     resource_identifiers = data['cloudformation']['resource_identifiers']
     sanitized_template = deepcopy(template)
+    logging.info(f'Sanitizing resources for creating change set...')
     for drifted_resource in drifts:
         resource_identifier = {}
 
@@ -181,19 +180,24 @@ def sanitize_resources(data, drifts, template):
             'Type': drifted_resource['ResourceType'],
             'Properties': json.loads(drifted_resource['ActualProperties'])
         }
+        logging.info(f'Updating stack resource: {drifted_resource["LogicalResourceId"]}')
+        logging.debug(f'Updating stack resource as follows: '
+                      f'{sanitized_template["Resources"][drifted_resource["LogicalResourceId"]]}')
 
         import_resources.append({
             'ResourceType': drifted_resource['ResourceType'],
             'LogicalResourceId': drifted_resource['LogicalResourceId'],
             'ResourceIdentifier': resource_identifier
         })
+        logging.info(f'Added the following to import resources: '
+                     f'{import_resources[import_resource_counter]}')
+        import_resource_counter += 1
     return sanitized_template, import_resources
 
 
 def set_resource_retention(template, supported_resources):
     retain_template = deepcopy(template)
-    pprint(supported_resources)
-    for resource in supported_resources:
+    for resource in supported_resources.keys():
         retain_template['Resources'][resource]['DeletionPolicy'] = 'Retain'
         logging.info(f'Added Retain Deletion Policy to resource: {resource}')
 
